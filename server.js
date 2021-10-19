d37948125a68788aa105fda84d2292efcb0d4adb
cpmascarenhas <cpmascarenhas@rd.com.br>
2021-10-19 20:15:34 -0300

    error listen

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = 8000;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.svg'));
 app.use(express.static(__dirname));
@@ -9,4 +8,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(process.env.PORT || 5000)
